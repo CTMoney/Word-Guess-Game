cffd030190de3094522e2e43c0d5b1edee1438e0
Cat <money1062@live.com>
2018-12-13 17:03:18 -0500

    non working current

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,154 +17,103 @@ Refactoring:
 /Refactoring
 */}
 
-//all required variables
+{//bonus
+    //  var hard = () => { tries = 7 }, normal = () => { tries = 13 }, easy = () => { tries = 19 };
+}
 
-//Containers to print info into
+var losses = 0, wins = 0;
+//push element out of randomWord
+// Document.ready equivalent for non jquery
+document.addEventListener("DOMContentLoaded", function start(event) {
+
+    var words = ["celebration", "sweet", "establish", "leave", "disturbance", "building", "annual", "murder", "salesperson", "pastel", "agree",
+        "allocation", "ecstasy", "sheet", "inspector", "multimedia", "earthflax", "air", "error", "coach"], wordBlank = " ";
+
+    var userGuesses = [], userGuess = "", incorrectGuesses = [], tries = 13;
+
+    document.getElementById("triesP").innerHTML = `Tries: ${tries}`;
 
 
-//word pool + word blank
-var words = ["celebration", "sweet", "establish", "leave", "disturbance", "building", "annual", "murder", "salesperson", "pastel", "agree",
-    "allocation", "ecstasy", "sheet", "inspector", "multimedia", "earthflax", "air", "error", "coach"], wordBlank = "";
-
-//guess array + current guess + incorrect guesses
-var userGuesses = [], userGuess = "", incorrectGuesses = [];
-
-//wins + losses + tries remaining
-var losses = 0, wins = 0, tries = 13;
-
-//Difficulty? add condition confirm + reset if confirm == true then tie to button
-var hard = () => { tries = 7 }, normal = () => { tries = 13 }, easy = () => { tries = 19 };
-
-//document.ready equivalent for non jquery
-document.addEventListener("DOMContentLoaded", function Start(event) {
-
-    //I tried putting this in the global scope and it didn't work which is kind of amusing
-    var feBlank = document.getElementById("blankW");
-
-    // Choosing the word
     randomWord = words[Math.floor(Math.random() * words.length) + 0];
+    randomCheck = randomWord;
+    console.log(randomWord);
 
-    // Generating a blank for user
-    function genBlank() {
-        for (i = 0; i < randomWord.length; i++) {
-            wordBlank += "_ ";
+    function generateBlank() {
+        for (i = 0; i < randomWord.length - 1; i++) {
+            wordBlank += "_";
         }
         document.getElementById("blankW").innerHTML = wordBlank;
     }
-    genBlank();
+    generateBlank();
 
-    // when a key is pressed itll store it as userGuess in lower case
-    // it will run function clicked which hits the visual button in the html 
     document.onkeyup = function (event) {
-        userGuess = event.key.toLowerCase();
-        //clicked(`l${userGuess.touppercase()}`);
-    }
 
-    function clicked(clicked_id) {
-        //the different button clicks contained in switch function
-        switch (clicked_id) {
-            case "lQ":
-                userGuess = "q";
-                break;
-            case "lW":
-                userGuess = "w";
-                break;
-            case "lE":
-                userGuess = "e";
-                break;
-            case "lR":
-                userGuess = "r";
-                break;
-            case "lT":
-                userGuess = "t";
-                break;
-            case "lY":
-                userGuess = "y";
-                break;
-            case "lU":
-                userGuess = "u";
-                break;
-            case "lI":
-                userGuess = "i";
-                break;
-            case "lO":
-                userGuess = "o";
-                break;
-            case "lP":
-                userGuess = "p";
-                break;
-            case "lA":
-                userGuess = "a";
-                break;
-            case "lS":
-                userGuess = "s";
-                break;
-            case "lD":
-                userGuess = "d";
-                break;
-            case "lF":
-                userGuess = "f";
-                break;
-            case "lG":
-                userGuess = "g";
-                break;
-            case "lH":
-                userGuess = "h";
-                break;
-            case "lJ":
-                userGuess = "j";
-                break;
-            case "lK":
-                userGuess = "k";
-                break;
-            case "lL":
-                userGuess = "l";
-                break;
-            case "lZ":
-                userGuess = "z";
-                break;
-            case "lX":
-                userGuess = "x";
-                break;
-            case "lC":
-                userGuess = "c";
-                break;
-            case "lV":
-                userGuess = "v";
-                break;
-            case "lB":
-                userGuess = "b";
-                break;
-            case "lN":
-                userGuess = "n";
-                break;
-            case "lM":
-                userGuess = "m";
-                break;
+        userGuess = event.key.toLowerCase();
+        var userGuessIndex = randomWord.indexOf(userGuess);
+
+
+        if (randomWord.includes(userGuess)) {
+            function updateBlank() {
+                wordBlank = wordBlank.replace(wordBlank.charAt(userGuessIndex), userGuess);
+                randomWord = randomWord.replace(wordBlank[userGuessIndex], '_');
+                document.getElementById("blankW").innerHTML = wordBlank;
+                console.log(randomWord);
+            }
+            updateBlank();
 
         }
+        else if (!randomWord.includes(userGuess)) {
+            function updateIncorrect() {
+                incorrectGuesses.push(userGuess);
+                document.getElementById("incorrectGuessP").innerHTML = incorrectGuesses;
+                tries--;
+                document.getElementById("triesP").innerHTML = `Tries: ${tries}`
+            }
+            updateIncorrect();
+
+        }
+        function winConditional() {
+            if (tries > 0 && wordBlank == randomCheck) {
+                wins++;
+                document.getElementById("winsP").innerHTML = `Wins: ${wins}`;
+                start();
+            }
+        }
+        winConditional();
+
+        function lossConditional() {
+            if (tries == 0) {
+                losses++
+                wordBlank = randomWord;
+                document.getElementById("lossesP").innerHTML = `Losses: ${losses}`
+                document.getElementById("blankW").innerHTML = wordBlank;
+                start();
+            }
+        }
+        lossConditional();
+
     }
 
-    //Reset Button
-    function newWordWGG() {
+    // Full reset Button
+    function resetWGG() {
         randomWord = words[Math.floor(Math.random() * words.length) + 0];
-        userGuesses = [], userGuess = "", IncorrectGuesses = [], wordBlank = "";
+        userGuesses = [], userGuess = "", IncorrectGuesses = [], wordBlank = "", losses = 0, wins = 0, tries = 13;
         start();
     }
-    //console.log("hello".includes("e"));
 
 });
 
+{//console.log("hello".includes("e"));
 /* still to-do
 
 var x = randomWord.indexOf(userGuess)
 
-if randomWord.indexOf(userGuess) !== -1 
+if randomWord.indexOf(userGuess) !== -1
 wordBlank.splice x=>userGuess
 innerhtml = wordBlank;
 
-if userGuess == -1 
-incorrectGuesses.push=>x 
+if userGuess == -1
+incorrectGuesses.push=>x
 document.elementID(button).style => crossed-out
 + button disabled
 tries--
@@ -179,4 +128,138 @@ if tries == 0
 losses++
 wordBlank = randomWord
 
+Bonus -Visual click on physical click
+    // clicked(`l${userGuess.toUpperCase()}`);
+    // console.log(`l${userGuess.toUpperCase()}`);
 */
+
+  // // Win conditional
+    // if (tries > 0 && wordBlank == randomWord) {
+    //     wins++
+    //     document.getElementById("winsP").innerHTML = `Wins: ${wins}`;
+
+    // }
+    // // Loss conditional
+    // if (tries == 0) {
+    //     losses++ 
+    //     wordBlank = randomWord;
+    //     document.getElementById("lossesP").innerHTML = `Losses: ${losses}`;
+    // }
+
+/* function clicked(clicked_id) {
+//     // The different button clicks contained in switch function
+//     switch (clicked_id) {
+//         case "lQ":
+//             userGuess = "q";
+//             break;
+//         case "lW":
+//             userGuess = "w";
+//             break;
+//         case "lE":
+//             userGuess = "e";
+//             break;
+//         case "lR":
+//             userGuess = "r";
+//             break;
+//         case "lT":
+//             userGuess = "t";
+//             break;
+//         case "lY":
+//             userGuess = "y";
+//             break;
+//         case "lU":
+//             userGuess = "u";
+//             break;
+//         case "lI":
+//             userGuess = "i";
+//             break;
+//         case "lO":
+//             userGuess = "o";
+//             break;
+//         case "lP":
+//             userGuess = "p";
+//             break;
+//         case "lA":
+//             userGuess = "a";
+//             break;
+//         case "lS":
+//             userGuess = "s";
+//             break;
+//         case "lD":
+//             userGuess = "d";
+//             break;
+//         case "lF":
+//             userGuess = "f";
+//             break;
+//         case "lG":
+//             userGuess = "g";
+//             break;
+//         case "lH":
+//             userGuess = "h";
+//             break;
+//         case "lJ":
+//             userGuess = "j";
+//             break;
+//         case "lK":
+//             userGuess = "k";
+//             break;
+//         case "lL":
+//             userGuess = "l";
+//             break;
+//         case "lZ":
+//             userGuess = "z";
+//             break;
+//         case "lX":
+//             userGuess = "x";
+//             break;
+//         case "lC":
+//             userGuess = "c";
+//             break;
+//         case "lV":
+//             userGuess = "v";
+//             break;
+//         case "lB":
+//             userGuess = "b";
+//             break;
+//         case "lN":
+//             userGuess = "n";
+//             break;
+//         case "lM":
+//             userGuess = "m";
+//             break;
+
+//     }
+//     let x = randomWord.indexOf(userGuess);
+//     if (randomWord.includes(userGuess)) {
+//         function updateBlank() {
+//             wordBlank = wordBlank.replace(wordBlank[x], userGuess);
+//             document.getElementById("blankW").innerHTML = wordBlank;
+
+//         }
+//         updateBlank();
+//     }
+//     else if (!randomWord.includes(userGuess)) {
+//         function updateIncorrect() {
+//             incorrectGuesses = incorrectGuesses.push(userGuess);
+//             document.getElementById("incorrectGuessP").innerHTML = incorrectGuesses;
+//             tries--;
+//             document.getElementById("triesP").innerHTML = `Tries: ${tries}`
+//         }
+//         updateIncorrect();
+
+//     }
+//     function winConditional() {
+//         if (tries > 0 && wordBlank == randomWord) {
+//             wins++
+//             document.getElementById("winsP").innerHTML = `Wins: ${wins}`;
+//         }
+//     }
+//     function lossConditional() {
+//         if (tries == 0) {
+//             losses++
+//             wordBlank = randomWord;
+//             document.getElementById("lossesP").innerHTML = `Losses: ${losses}`
+//             document.getElementById("").innerHTML = wordBlank;
+//         }
+//     }
+ } */}
